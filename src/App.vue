62b439bb1abacb8316cfe72b67d59a26bd4f33ac
Sarang S <email.sarangs@gmail.com>
2024-04-06 21:59:11 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Institutes from './components/Institutes.vue'
+</script>
+
+<template>
+  <h1>Open CS Rankings</h1>
+  <Suspense>
+    <template #default>
+      <Institutes />
+    </template>
+
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+  </Suspense>
+</template>
