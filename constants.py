76fe12a1f272d3f3f192f1fd55a088893c0531cd
BitC3t <bitc3t@gmail.com>
2024-04-07 05:37:58 +0530

    additional changes by joel

--- constants.py
+++ constants.py
@@ -1,23 +1,275 @@
-PUBLICATION_VENUES = {
-    "HPDC": "hpdc",
-    "FAST": "fast",
+PUBLICATION_VENUES = {  # (long_name: short_name)
+    "AAAI": "aaai",
+    "AAAI/IAAI": "aaai",
+    #
+    "IJCAI": "ijcai",
+    #
+    "CVPR": "cvpr",
+    "CVPR (1)": "cvpr",
+    "CVPR (2)": "cvpr",
+    #
+    "ECCV (1)": "eccv",
+    "ECCV (2)": "eccv",
+    "ECCV (3)": "eccv",
+    "ECCV (4)": "eccv",
+    "ECCV (5)": "eccv",
+    "ECCV (6)": "eccv",
+    "ECCV (7)": "eccv",
+    "ECCV (8)": "eccv",
+    "ECCV (9)": "eccv",
+    "ECCV (10)": "eccv",
+    "ECCV (11)": "eccv",
+    "ECCV (12)": "eccv",
+    "ECCV (13)": "eccv",
+    "ECCV (14)": "eccv",
+    "ECCV (15)": "eccv",
+    "ECCV (16)": "eccv",
+    "ECCV (17)": "eccv",
+    "ECCV (18)": "eccv",
+    "ECCV (19)": "eccv",
+    "ECCV (20)": "eccv",
+    "ECCV (21)": "eccv",
+    "ECCV (22)": "eccv",
+    "ECCV (23)": "eccv",
+    "ECCV (24)": "eccv",
+    "ECCV (25)": "eccv",
+    "ECCV (26)": "eccv",
+    "ECCV (27)": "eccv",
+    "ECCV (28)": "eccv",
+    "ECCV (29)": "eccv",
+    "ECCV (30)": "eccv",
+    "ECCV (31)": "eccv",
+    "ECCV (32)": "eccv",
+    "ECCV (33)": "eccv",
+    "ECCV (34)": "eccv",
+    "ECCV (35)": "eccv",
+    "ECCV (36)": "eccv",
+    "ECCV (37)": "eccv",
+    "ECCV (38)": "eccv",
+    "ECCV (39)": "eccv",
+    #
+    "ICCV": "iccv",
+    #
+    "ICLR": "iclr",
+    "ICLR (Poster)": "iclr",
+    #
+    "ICML": "icml",
+    "ICML (1)": "icml",
+    "ICML (2)": "icml",
+    "ICML (3)": "icml",
+    #
+    "KDD": "kdd",
+    #
+    "NeurIPS": "nips",
+    "NIPS": "nips",
+    #
+    "ACL": "acl",
+    "ACL (1)": "acl",
+    "ACL (2)": "acl",
+    "ACL/IJCNLP": "acl",
+    "ACL/IJCNLP (1)": "acl",
+    "ACL/IJCNLP (2)": "acl",
+    "COLING-A CL": "acl",
+    #
+    "EMNLP": "emnlp",
+    "EMNLP (1)": "emnlp",
+    "EMNLP-CoNLL": "emnlp",
+    "EMNLP-IJCNLP": "emnlp",
+    "EMNLP/IJCNLP (1)": "emnlp",
+    "HLT/EMNLP": "emnlp",
+    #
+    "NAACL": "naacl",
+    "NAACL-HLT": "naacl",
+    "NAACL-HLT (1)": "naacl",
+    "HLT-NAACL": "naacl",
+    #
+    "SIGIR": "sigir",
+    #
+    "WWW": "www",
+    #
+    "ISMB": "ismb",
+    "Bioinform.": "ismb",
+    "Bioinformatics": "ismb",
+    "Bioinformatics [ISMB/ECCB]": "ismb",
+    "ISMB (Supplement of Bioinformatics)": "ismb",
+    "ISMB/ECCB (Supplement of Bioinformatics)": "ismb",
+    #
+    "RECOMB": "recomb",
+    #
+    "EC": "ec",
+    #
+    "WINE": "wine",
+    #
+    "SIGCSE": "sigcse",
+    #
+    "Comput. Graph. Forum": "eurographics",
+    "EUROGRAPHICS": "eurographics",
+    #
+    "ACM Trans. Graph.": "siggraph",
+    "SIGGRAPH": "siggraph",
+    #
+    "SIGGRAPH Asia": "siggraph-asia",
+    #
+    "CHI": "chiconf",
+    #
     "UbiComp": "ubicomp",
     "Ubicomp": "ubicomp",
-    "Pervasive": "ubicomp",
     "IMWUT": "ubicomp",
+    "Pervasive": "ubicomp",
     "Proc. ACM Interact. Mob. Wearable Ubiquitous Technol.": "ubicomp",
-    "CCS": "ccs",
-    "IMC": "imc",
-    "NDSS": "ndss",
-    "PoPETs": "pets",
-    "Proc. Priv. Enhancing Technol.": "pets",
-    "USENIX Security": "usenixsec",
-    "USENIX Security Symposium": "usenixsec",
-    "PLDI": "pldi",
-    "POPL": "popl",
+    #
+    "UIST": "uist",
+    #
+    "ICRA": "icra",
+    "ICRA (1)": "icra",
+    "ICRA (2)": "icra",
+    #
+    "IROS": "iros",
+    #
+    "Robotics: Science and Systems": "rss",
+    #
+    "IEEE Visualization": "vis",
+    "IEEE Trans. Vis. Comput. Graph.": "vis",
+    #
+    "VR": "vr",
+    #
+    "ASPLOS": "asplos",
+    "ASPLOS (1)": "asplos",
+    "ASPLOS (2)": "asplos",
+    "ASPLOS (3)": "asplos",
+    #
+    "HPCA": "hpca",
+    #
+    "ISCA": "isca",
+    #
+    "MICRO": "micro",
+    #
+    "ICDE": "icde",
+    #
+    "PODS:": "pods",
+    #
+    "SIGMOD Conference": "sigmod",
+    "Proc. ACM Manag. Data": "sigmod",
+    #
+    "VLDB": "vldb",
+    "PVLDB": "vldb",
+    "Proc. VLDB Endow.": "vldb",
+    #
     "DAC": "dac",
-    "KDD": "kdd",
-}  # {long_name: short_name}
+    #
+    "ICCAD": "iccad",
+    #
+    "EMSOFT": "emsoft",
+    "ACM Trans. Embedded Comput. Syst.": "emsoft",
+    "ACM Trans. Embed. Comput. Syst.": "emsoft",
+    "IEEE Trans. Comput. Aided Des. Integr. Circuits Syst.": "emsoft",
+    #
+    "RTAS": "rtas",
+    "IEEE Real-Time and Embedded Technology and Applications Symposium": "rtas",
+    #
+    "RTSS": "rtss",
+    "rtss": "rtss",
+    #
+    "HPDC": "hpdc",
+    #
+    "ICS": "ics",
+    #
+    "SC": "sc",
+    #
+    "IMC": "imc",
+    "Internet Measurement Conference": "imc",
+    #
+    "SIGMETRICS": "sigmetrics",
+    "SIGMETRICS/Performance": "sigmetrics",
+    "POMACS": "sigmetrics",
+    "Proc. ACM Meas. Anal. Comput. Syst.": "sigmetrics",
+    #
+    "MobiCom": "mobicom",
+    "MOBICOM": "mobicom",
+    #
+    "MobiSys": "mobisys",
+    #
+    "SenSys": "sensys",
+    #
+    "NSDI": "nsdi",
+    #
+    "SIGCOMM": "sigcomm",
+    #
+    "EuroSys": "eurosys",
+    #
+    "FAST": "fast",
+    #
+    "OSDI": "osdi",
+    #
+    "SOSP": "sosp",
+    #
+    "USENIX Annual Technical Conference": "usenixatc",
+    "USENIX Annual Technical Conference, General Track": "usenixatc",
+    #
+    "ICFP": "icfp",
+    #
+    "OOPSLA": "oopsla",
+    "OOPSLA/ECOOP": "oopsla",
+    "OOPSLA1": "oopsla",
+    "OOPSLA2": "oopsla",
+    #
+    "PLDI": "pldi",
+    #
+    "POPL": "popl",
+    #
+    "CCS": "ccs",
+    "ACM Conference on Computer and Communications Security": "ccs",
+    #
+    "NDSS": "ndss",
+    #
+    "IEEE Symposium on Security and Privacy": "oakland",
+    "SP": "oakland",
+    #
+    "PoPETs": "pets",
+    "Privacy Enhancing Technologies": "pets",
+    "Proc. Priv. Enhancing Technol.": "pets",
+    #
+    "USENIX Security Symposium": "usenixsec",
+    "USENIX Security": "usenixsec",
+    #
+    "ASE": "ase",
+    #
+    "SIGSOFT FSE": "fse",
+    "ESEC/SIGSOFT FSE": "fse",
+    #
+    "ICSE": "icse",
+    "ICSE (1)": "icse",
+    #
+    "ISSTA": "issta",
+    #
+    "FOCS": "focs",
+    #
+    "SODA": "soda",
+    #
+    "STOC": "stoc",
+    #
+    "CRYPTO": "crypto",
+    "CRYPTO (1)": "crypto",
+    "CRYPTO (2)": "crypto",
+    "CRYPTO (3)": "crypto",
+    "CRYPTO (4)": "crypto",
+    "CRYPTO (5)": "crypto",
+    #
+    "EUROCRYPT": "eurocrypt",
+    "EUROCRYPT (1)": "eurocrypt",
+    "EUROCRYPT (2)": "eurocrypt",
+    "EUROCRYPT (3)": "eurocrypt",
+    "EUROCRYPT (4)": "eurocrypt",
+    "EUROCRYPT (5)": "eurocrypt",
+    #
+    "CAV": "cav",
+    "CAV (1)": "cav",
+    "CAV (2)": "cav",
+    "CAV (3)": "cav",
+    #
+    "LICS": "lics",
+    "CSL-LICS": "lics",
+}
 
 
 AUTHORS = {
