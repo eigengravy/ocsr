901ff7bb14aa088be9040e38bdd283571f546dfd
BitC3t <bitc3t@gmail.com>
2024-04-07 05:33:30 +0530

    aliases & full details

---
+++ aliases.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+df_alias = pd.DataFrame(columns = ['alias', 'name', 'dept'])
+df_affiliation = None
+
+# Create affliation table
+def read_affliations():
+    return pd.read_csv('./examples/affiliations.csv')
+    
+def generate_aliases():
+    global df_alias, df_affiliation
+    
+    for index, row in df_affiliation.iterrows():
+        prof_name = row['name']
+        prof_uni = row['affiliation']
+        prof_homepage = row['homepage']
+        prof_scholar = row['scholarid']
+        
+        # find aliases
+        aliases = []
+        
+        # search the dataframe for same homepage or uni or scholar
+        for index, row in df_affiliation.iterrows():
+            if row['homepage'] == prof_homepage or row['scholarid'] == prof_scholar:
+                if row['affiliation'] == prof_uni:
+                    aliases.append(row['name'])
+                    
+        # remove aliases and name from df_affiliation
+        for it in aliases:
+            df_affiliation = df_affiliation[df_affiliation['name'] != it]
+                    
+        for it in aliases:
+            df_alias.loc[len(df_alias)] = [it, prof_name, prof_uni]
+
+def start_gen():
+    global df_alias, df_affiliation
+    
+    df_affiliation = read_affliations()
+    generate_aliases()
+  
+    df_alias.to_csv('aliases.csv', index = False)
+    # print("Aliases generation has successfully completed.")
+    return df_alias
+                    
+                    
+                
+        
